6e505a299050c251c1a3e2894bcf14fc7aeccdc6
kjjohnsen <kjjohnsen@users.noreply.github.com>
2023-09-07 18:46:10 -0400

    lowered the max_chunk, which was ausing issues on some wifi

--- banglejs2/watch.js
+++ banglejs2/watch.js
@@ -30,7 +30,7 @@ Graphics.prototype.setFontAnton = function(scale) {
   }
   let reading_config = false;
   let timezone = -4;
-  let max_chunk = 23040; //the esp has a buffer of 25000, so this leaves some room
+  let max_chunk = 9000; //the esp has a buffer of 25000, so this leaves some room
   let ble_mtu = 768; //this isn't actually the ble mtu, which is locked at 128.  This is 6*128, 
   let from_time = require("Storage").read("from_time");
   if(from_time === undefined){
@@ -425,7 +425,6 @@ Graphics.prototype.setFontAnton = function(scale) {
             E.setTimeZone(0); //set to utc before setting a utc time
             setTime(timestamp);
             E.setTimeZone(timezone); //now set to timezone
-
 
 
             //send back the from_time
