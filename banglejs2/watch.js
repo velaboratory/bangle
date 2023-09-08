2c38a8bc7968037313a271752d0a28f2c822beaa
kjjohnsen <kjjohnsen@users.noreply.github.com>
2023-09-08 15:01:41 -0400

    can upload code to the watch using our own protocol, and added a python script to do this during development

--- banglejs2/watch.js
+++ banglejs2/watch.js
@@ -16,7 +16,7 @@ Graphics.prototype.setFontAnton = function(scale) {
   }else{
     last_hrm_reading_time = parseInt(last_hrm_reading_time);
   }
-  let version = "36";
+  let version = "37";
   let movement_filename = "healthlog"+version; 
   let acceleration_filename = "accellog"+version;
   let hrm_files_filename = "hrmfileslog"+version;
@@ -29,6 +29,7 @@ Graphics.prototype.setFontAnton = function(scale) {
     config = JSON.parse(atob(config)); //stored as b64, read back and parse
   }
   let reading_config = false;
+  let reading_firmware = false;
   let timezone = -4;
   let max_chunk = 9000; //the esp has a buffer of 25000, so this leaves some room
   let ble_mtu = 768; //this isn't actually the ble mtu, which is locked at 128.  This is 6*128, 
@@ -306,6 +307,7 @@ Graphics.prototype.setFontAnton = function(scale) {
       NRF.on('disconnect', function(reason) { 
           syncing = false;
           reading_config = false;
+          reading_firmware = false;
           hrmRawFile = null; //if I interruped a heart rate measurement, we need to restart it.
       });
       NRF.setTxPower(8);
@@ -315,8 +317,10 @@ Graphics.prototype.setFontAnton = function(scale) {
   
   E.setConsole("Terminal", {force: true});
   let config_buffer = "";
-
+  let firmware_buffer = "";
   currentFileIndex = 0;
+
+  let firmware_file = undefined;
   Bluetooth.on('data', function(data) {
     if(reading_config){
       
@@ -332,6 +336,7 @@ Graphics.prototype.setFontAnton = function(scale) {
                //require("Storage").open(config_filename, "w").write(config_json);
                reading_config = false;
                Bluetooth.write(3); //got all data
+               config_buffer = "";
                load(); //restart
            }
            else{
@@ -339,6 +344,18 @@ Graphics.prototype.setFontAnton = function(scale) {
            }
        }
 
+    }else if(reading_firmware){
+        parts = data.split("\n");
+        firmware_file.write(parts[0]);
+        if(parts.length > 1){
+            print("got everything")
+            reading_firmware = false;
+            Bluetooth.write(4); //got all data
+            firmware_file = require("Storage").open("firmware","r");
+            program = atob(firmware_file.read(10000000));
+            E.setBootCode(program);
+            load();
+        }
     }else{
         if(data.charCodeAt(0) == 1){ 
             bytes_sent = 0;
@@ -417,6 +434,14 @@ Graphics.prototype.setFontAnton = function(scale) {
             reading_config = true;
             if(debug) print("config read");
         }
+        if(data.charCodeAt(0) == 4){
+            print("starting firmware upload")
+            firmware_file = require("Storage").open("firmware","w").erase();
+            firmware_file = require("Storage").open("firmware","a")
+            firmware_buffer = "";
+            reading_firmware = true;
+            if(debug) print("firmware read");
+        }
         if(data.charCodeAt(0) == 7){ //a sync is going to start
 
 
@@ -449,10 +474,6 @@ Graphics.prototype.setFontAnton = function(scale) {
 
   });
   setupServer();
-  
-  // Load widgets
-  //Bangle.loadWidgets(); No widgets
   draw();
-  //setTimeout(Bangle.drawWidgets,0);
   }
   
