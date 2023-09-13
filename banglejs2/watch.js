cd18445a104c80dff7310e08b8bbe17edd9c7c75
huggyvi <141773672+huggyvi@users.noreply.github.com>
2023-09-13 16:24:29 -0400

    Added Dog Animation
    Added dog animation, fixed some small warnings.

--- banglejs2/watch.js
+++ banglejs2/watch.js
@@ -1,4 +1,5 @@
 
+
 NRF.disconnect(); //disconnect at the start.  This removes issues associated with terminals
 Graphics.prototype.setFontAnton = function(scale) {
     g.setFontCustom(atob("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAf/gAAAAAAAAAAf/gAAAAAAAAAAf/gAAAAAAAAAAf/gAAAAAAAAAAf/gAAAAAAAAAAf/gAAAAAAAAAAf/gAAAAAAAAAAf/gAAAAAAAAAAf/gAAAAAAAAAAf/gAAAAAAAAAAf/gAAAAAAAAAAf/gAAAAAAAAAAf/gAAAAAAAAAAf/gAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAADgAAAAAAAAAAA/gAAAAAAAAAAP/gAAAAAAAAAH//gAAAAAAAAB///gAAAAAAAAf///gAAAAAAAP////gAAAAAAD/////gAAAAAA//////gAAAAAP//////gAAAAH///////gAAAB////////gAAAf////////gAAP/////////gAD//////////AA//////////gAA/////////4AAA////////+AAAA////////gAAAA///////wAAAAA//////8AAAAAA//////AAAAAAA/////gAAAAAAA////4AAAAAAAA///+AAAAAAAAA///gAAAAAAAAA//wAAAAAAAAAA/8AAAAAAAAAAA/AAAAAAAAAAAAgAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAD//////AAAAAB///////8AAAAH////////AAAAf////////wAAA/////////4AAB/////////8AAD/////////+AAH//////////AAP//////////gAP//////////gAP//////////gAf//////////wAf//////////wAf//////////wAf//////////wA//8AAAAAB//4A//wAAAAAAf/4A//gAAAAAAP/4A//gAAAAAAP/4A//gAAAAAAP/4A//wAAAAAAf/4A///////////4Af//////////wAf//////////wAf//////////wAf//////////wAP//////////gAP//////////gAH//////////AAH//////////AAD/////////+AAB/////////8AAA/////////4AAAP////////gAAAD///////+AAAAAf//////4AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAP/gAAAAAAAAAAP/gAAAAAAAAAAf/gAAAAAAAAAAf/gAAAAAAAAAAf/AAAAAAAAAAA//AAAAAAAAAAA/+AAAAAAAAAAB/8AAAAAAAAAAD//////////gAH//////////gAP//////////gA///////////gA///////////gA///////////gA///////////gA///////////gA///////////gA///////////gA///////////gA///////////gA///////////gA///////////gA///////////gAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAH/4AAAAB/gAAD//4AAAAf/gAAP//4AAAB//gAA///4AAAH//gAB///4AAAf//gAD///4AAA///gAH///4AAD///gAP///4AAH///gAP///4AAP///gAf///4AAf///gAf///4AB////gAf///4AD////gA////4AH////gA////4Af////gA////4A/////gA//wAAB/////gA//gAAH/////gA//gAAP/////gA//gAA///8//gA//gAD///w//gA//wA////g//gA////////A//gA///////8A//gA///////4A//gAf//////wA//gAf//////gA//gAf/////+AA//gAP/////8AA//gAP/////4AA//gAH/////gAA//gAD/////AAA//gAB////8AAA//gAA////wAAA//gAAP///AAAA//gAAD//8AAAA//gAAAP+AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAB/+AAAAAD/wAAB//8AAAAP/wAAB///AAAA//wAAB///wAAB//wAAB///4AAD//wAAB///8AAH//wAAB///+AAP//wAAB///+AAP//wAAB////AAf//wAAB////AAf//wAAB////gAf//wAAB////gA///wAAB////gA///wAAB////gA///w//AAf//wA//4A//AAA//wA//gA//AAAf/wA//gB//gAAf/wA//gB//gAAf/wA//gD//wAA//wA//wH//8AB//wA///////////gA///////////gA///////////gA///////////gAf//////////AAf//////////AAP//////////AAP/////////+AAH/////////8AAH///+/////4AAD///+f////wAAA///8P////gAAAf//4H///+AAAAH//gB///wAAAAAP4AAH/8AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAD/wAAAAAAAAAA//wAAAAAAAAAP//wAAAAAAAAB///wAAAAAAAAf///wAAAAAAAH////wAAAAAAA/////wAAAAAAP/////wAAAAAB//////wAAAAAf//////wAAAAH///////wAAAA////////wAAAP////////wAAA///////H/wAAA//////wH/wAAA/////8AH/wAAA/////AAH/wAAA////gAAH/wAAA///4AAAH/wAAA//+AAAAH/wAAA///////////gA///////////gA///////////gA///////////gA///////////gA///////////gA///////////gA///////////gA///////////gA///////////gA///////////gA///////////gA///////////gA///////////gAAAAAAAAH/4AAAAAAAAAAH/wAAAAAAAAAAH/wAAAAAAAAAAH/wAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAB//8AAA/////+B///AAA/////+B///wAA/////+B///4AA/////+B///8AA/////+B///8AA/////+B///+AA/////+B////AA/////+B////AA/////+B////AA/////+B////gA/////+B////gA/////+B////gA/////+A////gA//gP/gAAB//wA//gf/AAAA//wA//gf/AAAAf/wA//g//AAAAf/wA//g//AAAA//wA//g//gAAA//wA//g//+AAP//wA//g////////gA//g////////gA//g////////gA//g////////gA//g////////AA//gf///////AA//gf//////+AA//gP//////+AA//gH//////8AA//gD//////4AA//gB//////wAA//gA//////AAAAAAAH////8AAAAAAAA////AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAD//////gAAAAB///////+AAAAH////////gAAAf////////4AAB/////////8AAD/////////+AAH//////////AAH//////////gAP//////////gAP//////////gAf//////////wAf//////////wAf//////////wAf//////////wAf//////////4A//wAD/4AAf/4A//gAH/wAAP/4A//gAH/wAAP/4A//gAP/wAAP/4A//gAP/4AAf/4A//wAP/+AD//4A///wP//////4Af//4P//////wAf//4P//////wAf//4P//////wAf//4P//////wAP//4P//////gAP//4H//////gAH//4H//////AAH//4D/////+AAD//4D/////8AAB//4B/////4AAA//4A/////wAAAP/4AP////AAAAB/4AD///4AAAAAAAAAH/8AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA//AAAAAAAAAAA//gAAAAAAAAAA//gAAAAAAAAAA//gAAAAAAADgA//gAAAAAAP/gA//gAAAAAH//gA//gAAAAB///gA//gAAAAP///gA//gAAAD////gA//gAAAf////gA//gAAB/////gA//gAAP/////gA//gAB//////gA//gAH//////gA//gA///////gA//gD///////gA//gf///////gA//h////////gA//n////////gA//////////gAA/////////AAAA////////wAAAA///////4AAAAA///////AAAAAA//////4AAAAAA//////AAAAAAA/////4AAAAAAA/////AAAAAAAA////8AAAAAAAA////gAAAAAAAA///+AAAAAAAAA///4AAAAAAAAA///AAAAAAAAAA//4AAAAAAAAAA/+AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAD//gB///wAAAAP//4H///+AAAA///8P////gAAB///+f////4AAD///+/////8AAH/////////+AAH//////////AAP//////////gAP//////////gAf//////////gAf//////////wAf//////////wAf//////////wA///////////wA//4D//wAB//4A//wB//gAA//4A//gA//gAAf/4A//gA//AAAf/4A//gA//gAAf/4A//wB//gAA//4A///P//8AH//4Af//////////wAf//////////wAf//////////wAf//////////wAf//////////gAP//////////gAP//////////AAH//////////AAD/////////+AAD///+/////8AAB///8f////wAAAf//4P////AAAAH//wD///8AAAAA/+AAf//AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAH//gAAAAAAAAB///+AA/+AAAAP////gA//wAAAf////wA//4AAB/////4A//8AAD/////8A//+AAD/////+A///AAH/////+A///AAP//////A///gAP//////A///gAf//////A///wAf//////A///wAf//////A///wAf//////A///wA///////AB//4A//4AD//AAP/4A//gAB//AAP/4A//gAA//AAP/4A//gAA/+AAP/4A//gAB/8AAP/4A//wAB/8AAf/4Af//////////wAf//////////wAf//////////wAf//////////wAf//////////wAP//////////gAP//////////gAH//////////AAH/////////+AAD/////////8AAB/////////4AAAf////////wAAAP////////AAAAB///////4AAAAAD/////wAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAf/AAB/8AAAAAA//AAD/8AAAAAA//AAD/8AAAAAA//AAD/8AAAAAA//AAD/8AAAAAA//AAD/8AAAAAA//AAD/8AAAAAA//AAD/8AAAAAA//AAD/8AAAAAA//AAD/8AAAAAA//AAD/8AAAAAA//AAD/8AAAAAA//AAD/8AAAAAA//AAD/8AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=="), 46, atob("EiAnGicnJycnJycnEw=="), 78 + (scale << 8) + (1 << 16));
@@ -57,6 +58,7 @@ Graphics.prototype.setFontAnton = function(scale) {
   Bangle.setHRMPower(false,"myApp"); 
 
 
+    
   setWatch(function(e){
     if(!Bangle.isHRMOn()){
         startHRMonitor();
@@ -64,8 +66,15 @@ Graphics.prototype.setFontAnton = function(scale) {
         stopHRMonitor();
         drawWidgets();
     }
+  Bangle.buzz();
+  //stop low power
+  stopEverythingDog();
+  //start high power
+  fastDrawIntervalDog = setInterval(drawClockFace, fastUpdateTimeDog);
+  //stop high power after some amount of time
+  timeoutDog = setTimeout(stopFastDrawDog, awakeTimeDog);
+  stopTimeoutDog = setTimeout(stopEverythingDog, stopTimeDog);
   }, BTN1, {repeat:true});
-
 
   var movement_log_buffer = new ArrayBuffer(9);
 
@@ -212,28 +221,77 @@ Graphics.prototype.setFontAnton = function(scale) {
         g.setFontAlign(1, 0).setFont("6x8",2).drawString(""+E.getBattery(), 170, 10);
         g.setFontAlign(-1, 0).setFont("6x8",2).drawString(""+Bangle.getHealthStatus("day").steps, 5, 10);
   };
-
-  let drawClockFace = function(){
-    var x = g.getWidth() / 2;
-    var y = g.getHeight() / 2;
-    g.reset().clearRect(Bangle.appRect); // clear whole background (w/o widgets)
-    var date = new Date();
-    var hour = date.getHours() % 12;
-    var minuteStr = (""+date.getMinutes()).padStart(2,"0");
-
-    var timeStr = (hour == 0?"12":(""+hour)) + ":"+minuteStr;
-    //var timeStr = locale.time(date, 1); // Hour and minute
-    g.setFontAlign(0, 0).setFont("Anton").drawString(timeStr, x, y);
-    // Show date and day of week
-    var dow = date.getDay();
-    days_of_week = ["Sun","Mon","Tues","Wednes","Thurs","Fri","Satur"];
-    var dateStringFull = date.toString();
-    var parts = dateStringFull.split(" ");
-    var dateStr = parts[2]+" " + parts[1] + " " + parts[3]+"\n"+
-                  days_of_week[dow].toUpperCase()+"DAY";
-    g.setFontAlign(0, 0).setFont("6x8", 2).drawString(dateStr, x, y+48);
+  
+    // Draw time
+    // Load fonts
+    require("Font7x11Numeric7Seg").add(Graphics);
+    // position on screen
+    const X = 150, Y = 170;
     
+// Dog animation
+    var fastUpdateTimeDog = 500;
+    var slowUpdateTimeDog = 2000;
+    var stopTimeDog = 10000;
+    var awakeTimeDog = 4000;
+    var fastDrawIntervalDog;
+    var slowDrawIntervalDog;
+    var timeoutDog;
+    var stopTimeoutDog;
+    var dog_imgs =                   [require("heatshrink").decompress(atob("iMUwkBBpNEAAdAA4MEogBDCBdAAwQQBgMQCAsRiIdBCoIIBgMRCAQaDCAYxCCAohFKIoQBiMM5gACA4QZC8gYDFQYABPIIHFGRYQHOwxtCgiHCPYJLGJgIA=")),require("heatshrink").decompress(atob("i0UwkBBY8BiMRgEEogADBQwBDCqFABAICBBQcRCooJDBQVAgAKIGoQKTFY5sIAQMQ5gAC4ASCoAKBDoPkIgIGCCoQqDJ4JpBGw5aBFoQKFgkAJhEECgUQEAwUCGoMEV4Q0DBQLQGBQQ=")),require("heatshrink").decompress(atob("icUwkBBY8BiEAogADoEAiMAglEAIYSNoAHCCQURiASFiIABEIIYBBIIRBCQYdDCQg3CCQwlGLo4JBhnMAARTCDgfkDYUEE4IsCAAIdBBYQvEBAQ4GoAbGJwIIBQgx6DgiXCRIJUHRoQ="))];
+
+  var currentframe=0;
+    
+  let drawClockFace = function(){
+ //draw current frame of dog
+  g.clearRect(50,30,165,165);
+  g.drawImage(dog_imgs[currentframe],50,20,{scale:5} );
+  currentframe = (currentframe + 1)% dog_imgs.length;
+    
+  // work out how to display the current time
+  var d = new Date();
+  var h = d.getHours(), m = d.getMinutes();
+  var time = (" "+h).substr(-2) + ":" + m.toString().padStart(2,0);
+  // Reset the state of the graphics library
+  g.reset();
+  // draw the current time (4x size 7 segment)
+  g.setFont("7x11Numeric7Seg",4);
+  g.setFontAlign(1,1); // align right bottom
+  g.drawString(time, X, Y, true /*clear background*/);
+  
   };
+
+// Clear the screen once, at startup
+g.clear();
+
+   
+    //by default start watch in low power
+slowDrawIntervalDog = setInterval(drawClockFace, slowUpdateTimeDog);
+    
+    let stopFastDrawDog = function() {
+  //actually stop high power
+  clearInterval(fastDrawIntervalDog);
+  //make sure and start low power again!
+  slowDrawIntervalDog= setInterval(drawClockFace, slowUpdateTimeDog);
+};
+
+let stopEverythingDog = function() {
+  if(timeoutDog) {
+    clearTimeout(timeoutDog);
+  }
+  if(stopTimeoutDog) {
+    clearTimeout(stopTimeoutDog);
+  }
+  if(fastDrawIntervalDog) {
+    clearInterval(fastDrawIntervalDog);
+  }
+  if(slowDrawIntervalDog) {
+    clearInterval(slowDrawIntervalDog);
+  }
+};
+    
+
+    
+
 
   let drawSyncProgress = function(){
     var w = g.getWidth();
@@ -321,10 +379,10 @@ Graphics.prototype.setFontAnton = function(scale) {
   };
   
   E.setConsole("Terminal", {force: true});
-  let config_file_temp = undefined;
-  let config_file_temp_name = "config_file_temp"
+  let config_file_temp;
+  let config_file_temp_name = "config_file_temp";
   currentFileIndex = 0;
-  let firmware_file = undefined;
+  let firmware_file;
   Bluetooth.on('data', function(data) {
     if(reading_config){
         parts = data.split("\n");
@@ -340,7 +398,7 @@ Graphics.prototype.setFontAnton = function(scale) {
         parts = data.split("\n");
         firmware_file.write(parts[0]);
         if(parts.length > 1){
-            print("got everything")
+            print("got everything");
             reading_firmware = false;
             Bluetooth.write(4); //got all data
             firmware_file = require("Storage").open("firmware","r");
@@ -425,13 +483,13 @@ Graphics.prototype.setFontAnton = function(scale) {
             config_buffer = ""; 
             reading_config = true;
             config_file_temp = require("Storage").open(config_file_temp_name, "w").erase();
-            config_file_temp = require("Storage").open(config_file_temp_name,"a")
+            config_file_temp = require("Storage").open(config_file_temp_name,"a");
             if(debug) print("config read");
         }
         if(data.charCodeAt(0) == 4){
-            print("starting firmware upload")
+            print("starting firmware upload");
             firmware_file = require("Storage").open("firmware","w").erase();
-            firmware_file = require("Storage").open("firmware","a")
+            firmware_file = require("Storage").open("firmware","a");
             firmware_buffer = "";
             reading_firmware = true;
             if(debug) print("firmware read");
